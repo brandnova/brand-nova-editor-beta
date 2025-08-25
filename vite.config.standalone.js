a721ff7a53d0d206595585ac21d5f3566eef4875
Ijeoma Jahsway <uchennamebijay@gmail.com>
2025-08-25 12:55:44 +0100

    Implemented standalone build and gh-pages deployment

--- vite.config.standalone.js
+++ vite.config.standalone.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: "./",  // ✅ important for GitHub Pages
   plugins: [react()],
   build: {
     lib: {
@@ -9,12 +10,6 @@ export default defineConfig({
       name: "BrandNovaEditor",
       fileName: (format) => `brand-nova-editor.${format}.js`,
       formats: ["umd"],
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: {},
-      },
     },
     outDir: "dist-standalone",
     emptyOutDir: true,
